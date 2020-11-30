8eec117894ef2c674225278a41eba03efc873196
boredanyway <ben.strohmann@gmail.com>
2020-11-30 13:12:59 +0100

    tryin a draggable list for all posts saving changes

---
+++ BlazorDraggableList/BlazorDraggableListBase.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorDraggableList
+{
+    public partial class BlazorDraggableListBase<TItemGeneric> : ComponentBase
+    {
+
+        [Parameter] public List<TItemGeneric> Items { get; set; }
+
+        [Parameter] public string Class { get; set; }
+
+        [Parameter] public RenderFragment<TItemGeneric> DraggableItem { get; set; }
+
+    }
+}
