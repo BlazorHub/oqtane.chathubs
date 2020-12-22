0d8fd573fd661a1d1b2c79fcd09d211f75ebaafe
boredanyway <ben.strohmann@gmail.com>
2020-12-22 19:35:10 +0100

    updated mobile blazor to dotnet 5 preview

--- Shared/Models/JsRuntimeObjectRef.cs
+++ Shared/Models/JsRuntimeObjectRef.cs
@@ -1,15 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Text.Json.Serialization;
-using System.Threading.Tasks;
+﻿using System.Text.Json.Serialization;
 
 namespace Oqtane.Shared.Models
 {
     public class JsRuntimeObjectRef
     {
+
         [JsonPropertyName("__jsObjectRefId")]
         public int JsObjectRefId { get; set; }
+
     }
 }
