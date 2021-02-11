6da04ca60c6b44e6294960f450408726bbc73652
boredanyway <ben.strohmann@gmail.com>
2021-02-11 20:24:44 +0100

    continued blazor touch menu impl

---
+++ BlazorTouchMenu/BlazorTouchMenuItemModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorTouchMenu
+{
+    public class BlazorTouchMenuItemModel
+    {
+
+        public string Name { get; set; }
+
+        public string Href { get; set; }
+
+        public string Icon { get; set; }
+
+        public NavLinkMatch NavLinkMatch { get; set; }
+
+    }
+}
