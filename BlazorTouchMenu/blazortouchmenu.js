290f9c7a1f87b90a6dc02b7108837bc4b3979d88
boredanyway <ben.strohmann@gmail.com>
2021-02-12 09:39:20 +0100

    blazor touche menu project changes

--- BlazorTouchMenu/blazortouchmenu.js
+++ BlazorTouchMenu/blazortouchmenu.js
@@ -2,12 +2,14 @@
 
     document.addEventListener('swiped-right', function (e) {
 
-        document.getElementById('blazortouchmenuid').style.display = "block";
+        document.getElementById('blazortouchmenuid').classList.remove('d-none');
+        document.getElementById('blazortouchmenuid').classList.add('d-block');
     });
 
     document.addEventListener('swiped-left', function (e) {
 
-        document.getElementById('blazortouchmenuid').style.display = "none";
+        document.getElementById('blazortouchmenuid').classList.remove('d-block');
+        document.getElementById('blazortouchmenuid').classList.add('d-none');
     });
 
 });
